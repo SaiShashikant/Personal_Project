88e75f21495c3173f32de1b8551ec1d0281f1459
Sai Shashikant <104009626+SaiShashikant@users.noreply.github.com>
2024-08-12 11:18:58 +0530

    Links added

--- src/app/lib/AppConstants.ts
+++ src/app/lib/AppConstants.ts
@@ -180,13 +180,17 @@ export const socialMedia = [
     {
         id: 1,
         img: "/git.svg",
+        link: "https://github.com/SaiShashikant",
     },
     {
         id: 2,
         img: "/twit.svg",
+        link: "https://x.com/crazyatom24",
     },
     {
         id: 3,
         img: "/link.svg",
+        link: "https://www.linkedin.com/in/shashikant987/",
+
     },
 ];
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import {MagicButton} from "@/components/ui/MagicButton";
 import {FaLocationArrow} from "react-icons/fa";
 import {socialMedia} from "@/app/lib/AppConstants";
+import Link from "next/link";
 
 export const Footer = () => {
     return (
@@ -20,14 +21,16 @@ export const Footer = () => {
             </div>
             <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
                 <p className="md:text-base text-sm md:font-normal font-light"> Copyright © 2024</p>
-            <div className="flex items-center md:gap-3 md:mt-0 mt-5 gap-6">
-                {socialMedia.map((profile)=> (
-                    <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
-                    <img src={profile.img} alt={profile.img}
-                         width={20} height={20}/>
-                    </div>
-                ))}
-            </div>
+                <div className="flex items-center md:gap-3 md:mt-0 mt-5 gap-6">
+                    {socialMedia.map((profile) => (
+                        <Link href={profile.link}>
+                            <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
+                                <img src={profile.img} alt={profile.img}
+                                     width={20} height={20}/>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </footer>
     )
